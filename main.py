c70af8e6fa657d11bfb27ce69d5f6e4a70c84ba1
Jean Victor Rada <radajean36@gmail.com>
2021-05-30 09:09:53 -0400

    https://www.youtube.com/watch?v=7t2alSnE2-I

---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'Data':{'Name':"Jean"}}
+
+@app.get('/about')
+def about():
+    return {'Data_about':{"Pagina acerca...":[{'1':'Jean', "2":'Victor', '3':'Rada'}]}}
+
+@app.get('/blog')
+def blog(limit=10, published:bool=True, sort: Optional[str]=None):
+
+    if published:
+        return {'Data':f"{limit} post published"}
+    else:
+        return {"Data":"No published"}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {"Data":'All unplublished blog'}
+
+
+# ESTRUCTURA DINAMICA CON SHOW
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'Data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id:int , limit=30):
+    # Comentario del user {id}
+    return {'Data': {'a)':'1', 'b)':'2', 'c)':'3'}, "Limit":limit, 'ID':id}
+
+class Blog(BaseModel):
+
+    title : str
+    body : str
+    published : Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog:Blog): # blog:Blog
+    return {'data':f"{blog.title} is title of the Blog.\n{blog.body}"}
